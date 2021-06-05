853047d9c897ca6b448ccc3e7f0bc0257eda3b17
Admin <sappi.cd@gmail.com>
2021-06-04 18:39:32 -0700

    Created ScheduleActivity

---
+++ app/src/main/java/com/blucor/thecontractor/project/ScheduleActivity.java
@@ -0,0 +1,16 @@
+package com.blucor.thecontractor.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.blucor.thecontractor.R;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
