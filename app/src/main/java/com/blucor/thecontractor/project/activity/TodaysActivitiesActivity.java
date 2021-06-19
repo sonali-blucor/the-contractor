1714e16282d24fd053e910516a8d657aa4a48607
Sonali <sonali.blucor@gmail.com>
2021-06-19 21:15:02 +0530

    add today activities

---
+++ app/src/main/java/com/blucor/thecontractor/project/activity/TodaysActivitiesActivity.java
@@ -0,0 +1,16 @@
+package com.blucor.thecontractor.project.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.blucor.thecontractor.R;
+
+public class TodaysActivitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todays_activities);
+    }
+}
