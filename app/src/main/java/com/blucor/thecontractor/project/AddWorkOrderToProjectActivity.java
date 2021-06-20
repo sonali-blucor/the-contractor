3e566bc10a73281d98b4078b79f890f0bbd4b42f
Admin <sappi.cd@gmail.com>
2021-06-20 19:18:01 +0530

    Created activity for work order

---
+++ app/src/main/java/com/blucor/thecontractor/project/AddWorkOrderToProjectActivity.java
@@ -0,0 +1,16 @@
+package com.blucor.thecontractor.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.blucor.thecontractor.R;
+
+public class AddWorkOrderToProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_work_order_to_project);
+    }
+}
