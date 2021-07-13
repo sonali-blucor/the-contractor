acbc760d2b4d671398c1da7adee4cc3a6eed284d
Mansingh Suryavanshi <man90040@gmail.com>
2021-07-13 10:44:12 +0530

    desgind both activity display list , row file project list change for visibility gone to kmow more button

--- app/src/main/java/com/blucor/thecontractor/project/activity/BillingDetailsDisplayListActivity.java
+++ app/src/main/java/com/blucor/thecontractor/project/activity/BillingDetailsDisplayListActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.blucor.thecontractor.R;
 
 public class BillingDetailsDisplayListActivity extends AppCompatActivity {
+
+    double Work_description, Paid, Amount, Balance, Remarks,Total;
 
     ListView simpleList;
     String billingDetailsList[] = {"Work description", "Paid", "Amount", "Balance", "Remarks"};
@@ -17,8 +20,13 @@ public class BillingDetailsDisplayListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_billing_details_display_list);
 
-            simpleList = (ListView)findViewById(R.id.lvbillingDetails);
-           // ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.billing_details_row, R.id.txt_billing, billingDetailsList);
+
+            Total = Work_description+Paid+Amount+Balance;
+            TextView tv_total = findViewById(R.id.tv_total);
+            tv_total.setText("Total :    "+Total);
+
+          simpleList = (ListView)findViewById(R.id.lvbillingDetails);
+            //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.billing_details_row, R.id.txt_billing, billingDetailsList);
            // simpleList.setAdapter(arrayAdapter);
         }
     }
