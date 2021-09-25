1da77bbcfa9aba65597e7ba90ce9f46f980fcd6f
Sonali <sonali.blucor@gmail.com>
2021-09-25 15:09:06 +0530

    add client billing

---
+++ app/src/main/java/com/blucor/tcthecontractor/project/workorderbilling/ClientBillingActivity.java
@@ -0,0 +1,16 @@
+package com.blucor.tcthecontractor.project.workorderbilling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.blucor.tcthecontractor.R;
+
+public class ClientBillingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_billing);
+    }
+}
